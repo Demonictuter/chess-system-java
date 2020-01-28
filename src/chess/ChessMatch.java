a02c871897df2055e0ccdb62f36f4a8ea93e486b
Demonictuter <joaopaulochagas1982@gmail.com>
2020-01-28 10:08:31 -0300

    place pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,11 +1,14 @@
 package chess;
 import board_game.Board;
+import chess_Pieces.Rook;
+import board_game.Position;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch(){
         board = new Board(8, 8);
+        initialSetup();
     }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getCollumns()];
@@ -16,4 +19,16 @@ public class ChessMatch {
         }
         return mat;
     }
+    private void initialSetup(){
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new Rook(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(7, 4));
+    }
 }
+
+
+
+
+
+
+
